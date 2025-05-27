add38e1584800deff71633947f488233e3e8aa37
Anfoyr <115868032+Anfoyr@users.noreply.github.com>
2025-05-27 19:04:08 +0300

    Add files via upload

---
+++ Pages/PageAddProduct.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp4.AppDataFile;
+
+namespace WpfApp4.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PageAddProduct.xaml
+    /// </summary>
+    public partial class PageAddProduct : Page
+    {
+        bool LogicRb = false;
+        public PageAddProduct()
+        {
+            InitializeComponent();          
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Product product = new Product() {
+                    Title = TxtTitle.Text,
+                    Cost = Convert.ToDecimal(TxtCost.Text),
+                    Description = TxtDescription.Text,
+                    MainImagePath = TxtImage.Text,
+                    IsActive = LogicRb,
+                    Manufacturer = CmbxOwner.SelectedItem as Manufacturer
+                };
+
+                ConnectOdb.conObj.Product.Add(product);
+                ConnectOdb.conObj.SaveChanges();
+                MessageBox.Show("Получилось!", "Notification", MessageBoxButton.OK, MessageBoxImage.Information);
+
+            }
+
+            catch (Exception er)
+             {
+                MessageBox.Show(er.Message.ToString());
+            }
+        }
+    }
+}
